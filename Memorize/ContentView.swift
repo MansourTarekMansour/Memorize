1ce1a756271d3bf38b3d595b9b540b0cedb8501e
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2022-10-22 13:38:24 +0200

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by mansour tarek on 22/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
