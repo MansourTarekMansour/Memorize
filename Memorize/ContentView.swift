fbf3d0dae479f63595803ab1b71b33bd8be09e89
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2022-10-22 22:11:36 +0200

    RoundedRectangle

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+        }
     }
 }
+
+
+struct CardView: View {
+    var isFaceUp = true
+    var body: some View{
+        let shape  = RoundedRectangle(cornerRadius: 20.0)
+        if isFaceUp {
+            ZStack (alignment: .center){
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 5).foregroundColor(.blue)
+                Text("🐹").font(.largeTitle)
+            }.padding(.all)
+        } else {
+            RoundedRectangle(cornerRadius: 20.0).fill(.red).padding()
+        }
+        
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
