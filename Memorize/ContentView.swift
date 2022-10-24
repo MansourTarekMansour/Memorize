2ca93a2fa6e3c5b97ccff27aecc39abf41643238
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2022-10-24 19:22:52 +0200

    last updates
    Button
    ForEach
    Spacer()

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,32 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis =  ["🚗","🚕 ","🚙 ","🚌","🚗","🚕 ","🚙 ","🚌","🚗","🚕 ","🚙 ","🚌","🚗","🚕 ","🚙 ","🚌","🚗","🚕 ","🚙 ","🚌"]
+    @State var emogiCount = 4
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+        
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emogiCount], id: \.self){emoji in
+                    CardView(content: emoji, isFaceUp: true)
+                }
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                plus
+            }.padding(.horizontal)
+        }
+    }
+    
+    var remove: some View{
+        Button {
+            if emogiCount > 1 {
+                emogiCount -= 1
+            }
+        }label: {
+            Image(systemName: "minus.circle").font(.largeTitle).foregroundColor(.red)
+        }
+    }
+    var plus: some View{
+        Button {
+            if emogiCount < emojis.count {
+                emogiCount += 1
+            }
+        }label: {
+            Image(systemName: "plus.circle").font(.largeTitle).foregroundColor(.red)
         }
     }
 }
 
 
+
 struct CardView: View {
-    var isFaceUp = true
+    var content: String
+    @State var isFaceUp = true
     var body: some View{
         let shape  = RoundedRectangle(cornerRadius: 20.0)
-        if isFaceUp {
-            ZStack (alignment: .center){
+        ZStack (alignment: .center){
+            if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 5).foregroundColor(.blue)
-                Text("🐹").font(.largeTitle)
-            }.padding(.all)
-        } else {
-            RoundedRectangle(cornerRadius: 20.0).fill(.red).padding()
+                shape.stroke(lineWidth: 5).foregroundColor(.red)
+                Text(content).font(.largeTitle)
+                }
+         else {
+            RoundedRectangle(cornerRadius: 20.0).fill(.red)
+         }
+        }.padding(.all).onTapGesture {
+            isFaceUp = !isFaceUp
         }
-        
-        
     }
 }
 
